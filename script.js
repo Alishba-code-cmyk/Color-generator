e40234750e42aac75b365acf8b7d4377e648134b
Alishba-code-cmyk <itsalishba00@gmail.com>
2025-04-28 21:41:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 let btn= document.querySelector("button");
-let colorbox=tdocument.querySelector(".color-box");
+let colorbox=document.querySelector('.color-box');
 btn.addEventListener("click",function(){
     
-    colorbox.style.backgroundcolor=randomcolor();
+    
+    colorbox.style.backgroundColor=getrandomcolor();
     console.log("color updated");
 });
-function randomcolor(){
+function getrandomcolor(){
     let red=Math.floor(Math.random()*255);
     let green=Math.floor(Math.random()*255);
     let blue=Math.floor(Math.random()*255);
     let color=`rgb(${red},${green},${blue}`;
     return color;
 }
-
-// btn.addEventListener("click", function(){
-//     btn.style.backgroundColor="blue";
-    
-// });
